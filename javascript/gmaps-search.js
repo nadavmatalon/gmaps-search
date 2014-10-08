53dce2ea40d7e4ce464ee1c2c79dae361a46df61
Nadav <nadav.matalon@gmail.com>
2014-10-08 23:34:13 +0100

    added autocomplete function

--- javascript/gmaps-search.js
+++ javascript/gmaps-search.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
 
 	var map = new GMaps({
 	  	div: '#map',
-	  	zoom: 15,
+	  	zoom: 12,
 	  	mapTypeId: google.maps.MapTypeId.ROADMAP,
 	  	lat: 0,
 	  	lng: 0
 	});
+
+	var geocoder = new google.maps.Geocoder();
 
 	$('#search').on('click', function() {
 		$('#image-container').fadeOut(200).remove();
@@ -46,6 +48,27 @@ $(document).ready(function() {
 			$('#search').trigger('click');
 		};
 	});
+
+	$("#address").autocomplete({
+	    source: function(request, response) {
+	      	geocoder.geocode( {'address': request.term }, function(results, status) {
+	        	response($.map(results, function(item) {
+	          		return {
+	            		label: item.formatted_address, // appears in dropdown box
+	            		value: item.formatted_address, // inserted into input element when selected
+	            		geocode: item                  // all geocode data: used in select callback event
+	          		}
+	        	}));
+	      	});
+	    },
+    	select: function(event,ui) {
+      		update_ui(ui.item.value, ui.item.geocode.geometry.location);
+    	}
+	});
+
+	function update_ui(address, latLng) {
+		$('#address').autocomplete("close").val(address);
+	};
 
 	$('#container').css('transition', 'opacity, 0.5s');
 	$('#container').css('opacity', '1');
